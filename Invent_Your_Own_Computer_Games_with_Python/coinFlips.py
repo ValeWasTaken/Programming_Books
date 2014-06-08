99eed4347fb21acac79cb9824288a23cd5a6150c
Michael Arnold <valestrum@hotmail.com>
2014-06-08 17:13:31 -0400

    Created improved coinFlips.py inspired by book

---
+++ Invent_Your_Own_Computer_Games_with_Python/coinFlips.py
@@ -0,0 +1,21 @@
+# Please note: This program was only tested using Python 2.7
+import random
+
+def main():
+        userGuess = input("I will flip a coin 1000 times. Enter your guess on how many flips will result in heads:  ")
+
+        flips = 0
+        heads = 0
+        while (flips < 1000):
+                if (random.randint(0, 1) == 1):
+                        heads += 1
+                flips += 1
+
+                if (flips % 250 == 0 and flips != 1000):
+                        print("At %d there were %d head results!" % (flips, heads))
+        difference = (int(userGuess) - int(heads))
+        print('\nOut of 1000 coin tosses, heads came up ' + str(heads) + ' times!')
+        print('Your guess was ' + str(difference) + ' away from the result! Well done!')
+main()
+
+# Original code provided in book: http://inventwithpython.com/coinFlips.py
