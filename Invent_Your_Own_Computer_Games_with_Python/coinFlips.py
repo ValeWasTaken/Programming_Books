305fa59691c8a76c357b7b612e6e213bb0d04bd4
Michael Arnold <valestrum@hotmail.com>
2014-06-16 22:31:33 -0400

    Condensed code

--- Invent_Your_Own_Computer_Games_with_Python/coinFlips.py
+++ Invent_Your_Own_Computer_Games_with_Python/coinFlips.py
@@ -1,9 +1,7 @@
-# Please note: This program was only tested using Python 2.7
 import random
-
+# Please note: This program was only tested using Python 2.7
 def main():
         userGuess = input("I will flip a coin 1000 times. Enter your guess on how many flips will result in heads:  ")
-
         flips = 0
         heads = 0
         while (flips < 1000):
@@ -14,8 +12,6 @@ def main():
                 if (flips % 250 == 0 and flips != 1000):
                         print("At %d there were %d head results!" % (flips, heads))
         difference = (int(userGuess) - int(heads))
-        print('\nOut of 1000 coin tosses, heads came up ' + str(heads) + ' times!')
-        print('Your guess was ' + str(difference) + ' away from the result! Well done!')
+        print('\nOut of 1000 coin tosses, heads came up '+str(heads)+' times!')
+        print('Your guess was '+str(difference)+' away from the result! Well done!')
 main()
-
-# Original code provided in book: http://inventwithpython.com/coinFlips.py
