da03c22eec99f098dd087ec7c3ad24feb3d46a80
Michael Arnold <valestrum@hotmail.com>
2014-07-07 08:58:16 -0400

    Create beerSong.java

---
+++ Head_First_Java/beerSong.java
@@ -0,0 +1,25 @@
+public class beerSong {
+	public static void main (String[]args) 
+	{
+		int beerNum = 99;
+		String word = "bottles";
+		
+		while (beerNum > 0)
+		{
+			if(beerNum == 1)
+			{word = "bottle";} // Change wording if last bottle.
+			
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+			System.out.println(beerNum + " " + word + " of beer");
+			System.out.println("Take one down");
+			System.out.println("Pass it around");
+			beerNum--;
+		}
+		if(beerNum == 0)
+		{
+			System.out.println("No more bottles of beer on the wall");
+		}
+		else 
+		{System.out.println("Error.");}
+	}
+}
