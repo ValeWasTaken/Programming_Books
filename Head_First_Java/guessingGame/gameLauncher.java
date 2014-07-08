63f6f827fb0632fa4912724f7eabe9307a291dbf
Michael Arnold <valestrum@hotmail.com>
2014-07-08 11:07:48 -0400

    Update gameLauncher.java

--- Head_First_Java/guessingGame/gameLauncher.java
+++ Head_First_Java/guessingGame/gameLauncher.java
@@ -5,3 +5,5 @@ public class gameLauncher {
 		game.startGame();
 	}
 }
+// Note: This version of guessingGame is simply the introduction to a concept.
+// The program itself is not meant to be very useful or well-done. It is only to serve as an example.
