418d4466413aca640093c6daf94e1a17712c9286
Michael Arnold <valestrum@hotmail.com>
2015-07-30 22:44:15 -0400

    Forgot to add "total items" to the program. Fixed.

--- Automate_the_Boring_Stuff/Chapter_005/Practice_Problems/Fantasy_Game_Inventory.py
+++ Automate_the_Boring_Stuff/Chapter_005/Practice_Problems/Fantasy_Game_Inventory.py
@@ -8,6 +8,9 @@ inventory = {
     'arrow': 12
     }
 def display_inventory():
+    total_items = 0
     for item in inventory:
         print('{0}: {1}'.format(inventory[item], item))
+        total_items += inventory[item]
+    print('Total amount of items: {0}'.format(total_items))
 display_inventory()
