839731fc123d858d63f15311d92f534b0b770c7c
Michael Arnold <valestrum@hotmail.com>
2015-12-01 14:04:23 -0500

    Optimized my solution

--- Automate_the_Boring_Stuff/Chapter_007/Practice_Problems/Strong_Password_Detection.py
+++ Automate_the_Boring_Stuff/Chapter_007/Practice_Problems/Strong_Password_Detection.py
@@ -1,17 +1,24 @@
+# Chapter 7: Strong Password Detection - Python 3.4
+import re
+
 def is_password_strong():
+    """
+    Returns a boolean indicating if the password is strong.
+
+    Strong passwords are defined here as 8 characters or longer,
+    contain at least one uppercase letter,
+    lowercase letter and digit.
+    """
     password = input('Enter a password to test: ')
     
     length_regex = re.compile(r'.{8,}')
-    length = True if length_regex.search(password) != None else False
-
     uppercase_regex = re.compile(r'[A-Z]')
     lowercase_regex = re.compile(r'[a-z]')
-    uppercase = True if uppercase_regex.search(password) != None else False
-    lowercase = True if lowercase_regex.search(password) != None else False
-    case = True if uppercase and lowercase == True else False
-
     digit_regex = re.compile(r'[0-9]')
-    digit = True if digit_regex.search(password) != None else False
+    
+    length = length_regex.search(password) is not None
+    case = (uppercase_regex.search(password) is not None and
+        lowercase_regex.search(password) is not None)
+    digit = digit_regex.search(password) is not None
 
-    return(length and case and digit == True
-print(is_password_strong())
+    return(length and case and digit)
